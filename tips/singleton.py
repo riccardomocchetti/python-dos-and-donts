1763d29f516952db5ecb9fb66d3fb451e2112b49
Riccardo Mocchetti <riccardo@infinitelambda.com>
2022-06-30 14:17:14 +0100

    new tip: singleton pattern in python

---
+++ tips/singleton.py
@@ -0,0 +1,37 @@
+
+# Gang of Four implementation
+class A:
+    _instance = None
+
+    def __init__(self):
+        raise RuntimeError(f"Please call {__class__.__qualname__}.instance() instead")
+    
+    @classmethod
+    def instance(cls):
+        if not cls._instance:
+            print(f"Creating new instance of {__class__.__qualname__}")
+            cls._instance = cls.__new__(cls)
+            # initialise your instance here
+        return cls._instance
+
+
+# Pythonic implementation
+class B:
+    _instance = None
+
+    def __new__(cls):
+        if not cls._instance:
+            print(f"Creating new instance of {__class__.__qualname__}")
+            cls._instance = super().__new__(cls)
+            # initialise here
+        return cls._instance
+
+
+# Usage
+# a = A() # raises an error
+a = A.instance()
+a = A.instance()
+print(a)
+b = B()
+b = B()
+print(b)
